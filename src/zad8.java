5f692a8a5a38e300b260764a4be6ef58f2adce73
00 <00>
2022-10-06 12:31:02 +0200

    gfgfgk

---
+++ src/zad8.java
@@ -0,0 +1,17 @@
+public class zad8 {
+    public static void main(String[] args)
+    {
+        String name;
+        int age;
+        double annualPay;
+
+        name = "Marcin Lichon";
+        age = 17;
+        annualPay = 600000;
+
+        System.out.print("Nazywam się " + name + ", mam " + age + "\n  chcę zarabiać " + annualPay + " złotych rocznie.");
+
+    }
+}
+
+
